a3075a101fd33deb15eb3ba928ac78f8308369f6
Angel <angelbuenrostro@outlook.com>
2019-09-09 17:36:19 -0400

    Add Edge function

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -11,13 +11,15 @@ class Graph:
         """
         Add a vertex to the graph.
         """
-        self.vertices[vertex] = {}
-        
+        self.vertices[vertex] = set()
+
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
         """
-        pass  # TODO
+        if all (key in self.vertices for key in (v1, v2)):
+            self.vertices[v1].add(v2)
+
     def bft(self, starting_vertex):
         """
         Print each vertex in breadth-first order

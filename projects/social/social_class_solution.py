ec0231c184b4581c25c0a88acd67e7eeb060a1ae
Angel <angelbuenrostro@outlook.com>
2019-09-12 12:49:11 -0400

    Refactor populate graph to use tuples instead of strings to hold existing friendship connections

---
+++ projects/social/social_class_solution.py
@@ -0,0 +1,15 @@
+"""
+Create Friendships
+"""
+
+# Add Users
+
+for i in range(0, numUsers):
+    self.addUser(i)
+
+# Create friendships
+# Make List of all frienship possibilities
+possible_frienships = []
+# Shuffle friendship list
+
+# Pick N from beginning
